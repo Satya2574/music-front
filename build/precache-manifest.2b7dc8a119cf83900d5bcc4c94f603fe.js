faa90ce0c2dd77264f1d9bc180a96e1d68588658
SATYANARAYANA REDDY <satyanarayanareddy@SATYANARAYANAs-MacBook-Air.local>
2022-02-01 17:17:08 +0530

    first commit

---
+++ build/precache-manifest.2b7dc8a119cf83900d5bcc4c94f603fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6298b5f71ec7e52c9724285bf093f909",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1bdbfc20a502d975600",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0dce39f6345980ed8e05",
+    "url": "/static/css/main.5faf9026.chunk.css"
+  },
+  {
+    "revision": "c1bdbfc20a502d975600",
+    "url": "/static/js/2.d0557b47.chunk.js"
+  },
+  {
+    "revision": "b4ee14f1b0eb12541a4fed8ba73fd856",
+    "url": "/static/js/2.d0557b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dce39f6345980ed8e05",
+    "url": "/static/js/main.3b6592b8.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
